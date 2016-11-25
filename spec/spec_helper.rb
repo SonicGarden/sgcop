d70852f06b8365a832bfc5aabc19fb193196455f
aki <lala.akira@gmail.com>
2016-11-25 11:33:58 +0900

    rubocop のバージョンを 0.45.0 にアップデート

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sgcop'
 
-rubocop_path = Gem::Specification.find_by_name('rubocop').gem_dir
-Dir["#{rubocop_path}/spec/support/**/*.rb"].each { |f| require f }
+require 'rubocop/rspec/support'
