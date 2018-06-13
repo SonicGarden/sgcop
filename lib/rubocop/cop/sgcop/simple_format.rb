3540d610081a6735547bc95bce33e06932790e85
aki <lala.akira@gmail.com>
2018-06-13 11:56:32 +0900

    fix a bit

--- lib/rubocop/cop/sgcop/simple_format.rb
+++ lib/rubocop/cop/sgcop/simple_format.rb
@@ -8,9 +8,19 @@ module RuboCop
 
         def on_send(node)
           receiver, method_name, *args = *node
-          if receiver.nil? && method_name == :simple_format && args.first&.child_nodes&.empty?
+          if receiver.nil? && method_name == :simple_format && warning_args?(args)
             add_offense(node)
           end
+        end
+
+        private
+
+        def warning_args?(args)
+          return false if args.first.nil?
+          return true if args.first.child_nodes.empty?
+
+          receiver, *_args = *args.first.child_nodes.first
+          %i[object @object].member?(receiver)
         end
       end
     end
--- spec/rubocop/cop/sgcop/simple_format_spec.rb
+++ spec/rubocop/cop/sgcop/simple_format_spec.rb
@@ -12,6 +12,15 @@ describe RuboCop::Cop::Sgcop::SimpleFormat do
     RUBY
   end
 
+  it 'simple_formatにオブジェクトのメソッドが渡されていたら警告' do
+    expect_offense(<<~RUBY)
+      def html_format(object)
+        simple_format(object.body)
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^ simple_format does not escape HTML tags.
+      end
+    RUBY
+  end
+
   it 'simple_formatにメソッドの返り値が渡されていたら警告なし' do
     expect_no_offenses(<<~RUBY)
       def html_format(text)
@@ -20,11 +29,53 @@ describe RuboCop::Cop::Sgcop::SimpleFormat do
     RUBY
   end
 
-  it 'haml-lintでsimple_formatにローカス変数が直接渡されていたら警告' do
+  it 'haml-lintでsimple_formatにローカス変数が渡されていたら警告' do
     expect_offense(<<~RUBY)
       haml_lint_puts_0 # h1
       simple_format(text)
       ^^^^^^^^^^^^^^^^^^^ simple_format does not escape HTML tags.
+      _haml_lint_puts_1 # h1/
+    RUBY
+  end
+
+  it 'haml-lintでsimple_formatにインスタンス変数が渡されていたら警告' do
+    expect_offense(<<~RUBY)
+      haml_lint_puts_0 # h1
+      simple_format(@text)
+      ^^^^^^^^^^^^^^^^^^^^ simple_format does not escape HTML tags.
+      _haml_lint_puts_1 # h1/
+    RUBY
+  end
+
+  it 'haml-lintでsimple_formatにインスタンス変数のメソッドが渡されていたら警告' do
+    expect_offense(<<~RUBY)
+      haml_lint_puts_0 # h1
+      simple_format(@object.body)
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^ simple_format does not escape HTML tags.
+      _haml_lint_puts_1 # h1/
+    RUBY
+  end
+
+  it 'haml-lintでsimple_formatにヘルパ経由のローカル変数が渡されていたら警告なし' do
+    expect_no_offenses(<<~RUBY)
+      haml_lint_puts_0 # h1
+      simple_format(h(text))
+      _haml_lint_puts_1 # h1/
+    RUBY
+  end
+
+  it 'haml-lintでsimple_formatにヘルパ経由のインスタンス変数が渡されていたら警告なし' do
+    expect_no_offenses(<<~RUBY)
+      haml_lint_puts_0 # h1
+      simple_format(h(@text))
+      _haml_lint_puts_1 # h1/
+    RUBY
+  end
+
+  it 'haml-lintでsimple_formatにヘルパ経由のインスタンス変数のメソッドが渡されていたら警告なし' do
+    expect_no_offenses(<<~RUBY)
+      haml_lint_puts_0 # h1
+      simple_format(h(@object.body))
       _haml_lint_puts_1 # h1/
     RUBY
   end
