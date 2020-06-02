7fd30ae39e8b7192b74fd3405b7faa608fd782c0
aki <lala.akira@gmail.com>
2020-06-03 07:24:33 +0900

    feat: Add Sgcop/Sleep

---
+++ spec/rubocop/cop/sgcop/sleep_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe RuboCop::Cop::Sgcop::Sleep do
+  subject(:cop) { RuboCop::Cop::Sgcop::Sleep.new }
+
+  it 'registers an offense for an sleep call with no receiver' do
+    expect_offense(<<~RUBY)
+      it do
+        click_on 'button'
+        sleep 10
+        ^^^^^ Do not use `sleep` in spec.
+      end
+    RUBY
+  end
+
+  it 'does register an offense for explicit Kernel.sleep calls' do
+    expect_offense(<<~RUBY)
+      it do
+        click_on 'button'
+        Kernel.sleep 10
+               ^^^^^ Do not use `sleep` in spec.
+      end
+    RUBY
+  end
+
+  it 'does not register an offense for an explicit sleep call on an object' do
+    expect_no_offenses('Object.new.sleep')
+  end
+end
