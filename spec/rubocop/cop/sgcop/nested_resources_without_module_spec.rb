5581b4fe60308683a1d28e7cd42edc21e7ff010b
aki <lala.akira@gmail.com>
2025-08-12 10:57:06 +0900

    feat: Add NestedResourcesWithoutModule cop to enforce module option in nested resources routing

---
+++ spec/rubocop/cop/sgcop/nested_resources_without_module_spec.rb
@@ -0,0 +1,143 @@
+require 'spec_helper'
+
+describe RuboCop::Cop::Sgcop::NestedResourcesWithoutModule do
+  subject(:cop) { RuboCop::Cop::Sgcop::NestedResourcesWithoutModule.new }
+
+  context 'nested resources with module option' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[create], module: :posts
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'nested resources with namespace option' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[create], namespace: :posts
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'nested resources with scope option' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[create], scope: :posts
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'nested resources without module option' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[create]
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Sgcop/NestedResourcesWithoutModule: Use `module:` option in nested resource/resources routing.
+          end
+        end
+      RUBY
+    end
+
+    it 'registers an offense with other options' do
+      expect_offense(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[create], path: 'remarks'
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Sgcop/NestedResourcesWithoutModule: Use `module:` option in nested resource/resources routing.
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'nested resource (singular) without module option' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resource :comment, only: %i[create]
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Sgcop/NestedResourcesWithoutModule: Use `module:` option in nested resource/resources routing.
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'deeply nested resources' do
+    it 'registers offense for nested resources without module' do
+      expect_offense(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[index create], module: :posts do
+              resources :reactions, only: %i[create]
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Sgcop/NestedResourcesWithoutModule: Use `module:` option in nested resource/resources routing.
+            end
+          end
+        end
+      RUBY
+    end
+
+    it 'does not register offense when all have module' do
+      expect_no_offenses(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[index create], module: :posts do
+              resources :reactions, only: %i[create], module: :comments
+            end
+          end
+        end
+      RUBY
+    end
+  end
+
+  context 'top-level resources' do
+    it 'does not register an offense for non-nested resources' do
+      expect_no_offenses(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy]
+          resources :comments, only: %i[index create]
+        end
+      RUBY
+    end
+  end
+
+  context 'multiple nested resources in same block' do
+    it 'registers offense for each without module' do
+      expect_offense(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[create]
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Sgcop/NestedResourcesWithoutModule: Use `module:` option in nested resource/resources routing.
+            resources :likes, only: %i[create destroy]
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Sgcop/NestedResourcesWithoutModule: Use `module:` option in nested resource/resources routing.
+          end
+        end
+      RUBY
+    end
+
+    it 'registers offense only for those without module' do
+      expect_offense(<<~RUBY, 'config/routes.rb')
+        Rails.application.routes.draw do
+          resources :posts, only: %i[index show new edit create update destroy] do
+            resources :comments, only: %i[create], module: :posts
+            resources :likes, only: %i[create destroy]
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Sgcop/NestedResourcesWithoutModule: Use `module:` option in nested resource/resources routing.
+          end
+        end
+      RUBY
+    end
+  end
+end
