581d1de39ecbe8b4e951d8d872de0cc37692f0a8
aki <lala.akira@gmail.com>
2025-10-28 14:48:08 +0900

    refactor: case inを使うように見直し

--- lib/rubocop/cop/sgcop/rspec/active_job_test_helper.rb
+++ lib/rubocop/cop/sgcop/rspec/active_job_test_helper.rb
@@ -5,13 +5,12 @@ module RuboCop
     module Sgcop
       module Rspec
         class ActiveJobTestHelper < Base
-          MSG = 'Use ActiveJob::TestHelper methods instead of RSpec ActiveJob matchers.'
-          MSG_HAVE_ENQUEUED_JOB = 'Use `assert_enqueued_jobs(count)` or `assert_enqueued_with` instead of `%<matcher>s`.'
-          MSG_HAVE_BEEN_ENQUEUED = 'Use `assert_enqueued_with` instead of `%<matcher>s`.'
-          MSG_ENQUEUE_JOB = 'Use `assert_enqueued_jobs` with a block instead of `%<matcher>s`.'
-          MSG_HAVE_PERFORMED_JOB = 'Use `assert_performed_jobs(count)` or `assert_performed_with` instead of `%<matcher>s`.'
-          MSG_HAVE_BEEN_PERFORMED = 'Use `assert_performed_with` instead of `%<matcher>s`.'
-          MSG_PERFORM_JOB = 'Use `assert_performed_jobs` with a block instead of `%<matcher>s`.'
+          MSG_HAVE_ENQUEUED_JOB = 'Use `assert_enqueued_jobs(count)` or `assert_enqueued_with` instead of `have_enqueued_job`.'
+          MSG_HAVE_BEEN_ENQUEUED = 'Use `assert_enqueued_with` instead of `have_been_enqueued`.'
+          MSG_ENQUEUE_JOB = 'Use `assert_enqueued_jobs` with a block instead of `enqueue_job`.'
+          MSG_HAVE_PERFORMED_JOB = 'Use `assert_performed_jobs(count)` or `assert_performed_with` instead of `have_performed_job`.'
+          MSG_HAVE_BEEN_PERFORMED = 'Use `assert_performed_with` instead of `have_been_performed`.'
+          MSG_PERFORM_JOB = 'Use `assert_performed_jobs` with a block instead of `perform_job`.'
 
           def_node_matcher :active_job_matcher?, <<~PATTERN
             (send nil? ${:have_enqueued_job :have_been_enqueued :enqueue_job
@@ -29,20 +28,18 @@ module RuboCop
 
           def message_for_matcher(matcher_name)
             case matcher_name
-            when :have_enqueued_job
-              format(MSG_HAVE_ENQUEUED_JOB, matcher: 'have_enqueued_job')
-            when :have_been_enqueued
-              format(MSG_HAVE_BEEN_ENQUEUED, matcher: 'have_been_enqueued')
-            when :enqueue_job
-              format(MSG_ENQUEUE_JOB, matcher: 'enqueue_job')
-            when :have_performed_job
-              format(MSG_HAVE_PERFORMED_JOB, matcher: 'have_performed_job')
-            when :have_been_performed
-              format(MSG_HAVE_BEEN_PERFORMED, matcher: 'have_been_performed')
-            when :perform_job
-              format(MSG_PERFORM_JOB, matcher: 'perform_job')
-            else
-              MSG
+            in :have_enqueued_job
+              MSG_HAVE_ENQUEUED_JOB
+            in :have_been_enqueued
+              MSG_HAVE_BEEN_ENQUEUED
+            in :enqueue_job
+              MSG_ENQUEUE_JOB
+            in :have_performed_job
+              MSG_HAVE_PERFORMED_JOB
+            in :have_been_performed
+              MSG_HAVE_BEEN_PERFORMED
+            in :perform_job
+              MSG_PERFORM_JOB
             end
           end
         end
