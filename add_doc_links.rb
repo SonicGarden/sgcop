f2e294dad5aa4fc55636833bbf009ae135095287
aki <lala.akira@gmail.com>
2026-06-30 15:19:40 +0900

    fix: skip Sgcop/* custom cops in add_doc_links to avoid dead links
    Sgcop/* はカスタム Cop で docs.rubocop.org に掲載されないため、
    add_doc_links.rb の自動リンク生成対象に strict ファイルを加えた際、
    存在しない docs URL が手書きの GitHub リンクと重複して挿入されていた。
    custom_cop? で Sgcop/* を除外する。コメントアウトされた # Sgcop/... 行も
    先頭の # を取り除いてから判定することで併せて除外する。

--- add_doc_links.rb
+++ add_doc_links.rb
@@ -27,6 +27,13 @@ def doc_url(cop_name)
   end
 end
 
+# Sgcop/* はカスタム Cop で docs.rubocop.org に掲載されないため、自動リンク生成の
+# 対象外とする（GitHub リンクは yml 側に手書きで付与済み）。コメントアウトされた
+# `# Sgcop/...` 行も除外できるよう、先頭の `# ` を取り除いてから判定する。
+def custom_cop?(cop_name)
+  cop_name.sub(/\A#\s*/, '').start_with?('Sgcop/')
+end
+
 def add_doc_link_comments(filename)
   tmp_filename = "#{filename}.tmp"
   File.open(tmp_filename, 'w') do |io|
@@ -34,7 +41,7 @@ def add_doc_link_comments(filename)
       if DOC_COMMENT_REGEXP.match?(line)
         next
 
-      elsif (m = %r{^(.+/.+):}.match(line))
+      elsif (m = %r{^(.+/.+):}.match(line)) && !custom_cop?(m[1])
         io.puts(doc_url(m[1]))
       end
 
